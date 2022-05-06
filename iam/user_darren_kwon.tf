4f6e8fe1a5a234d8791f8d638969653a007a3029
DarrenKwonDev <anatomy1545@gmail.com>
2022-05-06 18:58:11 +0900

    feat: iam user policy

--- iam/user_darren_kwon.tf
+++ iam/user_darren_kwon.tf
@@ -10,3 +10,19 @@ resource "aws_iam_user" "darren-kwon" {
   # password는 aws cli를 통해서 만들거나 aws_iam_user_login_profile를 활용할 것. 여기선 cli로 진행
   #  https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_passwords_account-policy.html
 }
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_user_policy
+resource "aws_iam_user_policy" "name" {
+  name = "admin"
+  user = aws_iam_user.darren-kwon.name
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = ["*"],
+        Effect = "Allow",
+        Resource = ["*"]
+      }
+    ]
+  })
+}
